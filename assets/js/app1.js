281257468b8449fe89b8081c57e13b93a3da7ee4
SofinaeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-07-07 16:49:42 +0200

    commit

---
+++ assets/js/app1.js
@@ -0,0 +1,8 @@
+
+
+function menuToggle(){
+    let nav = document.getElementById('menu-overlay');
+    nav.classList.toggle('active');
+    let navv = document.getElementById('toggleIcon');
+    navv.classList.toggle('active');
+}
